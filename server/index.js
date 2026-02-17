a2fa25691c5f64317af483e456ea49692a9cffa5
Grayson Gales <graysongales15@gmail.com>
2026-02-16 23:07:26 -0500

    initial project setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+// If you don’t want ESM imports in Node, you can switch this to require(...). If you do want ESM, add "type": "module" to server/package.json.
+
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => res.json({ ok: true }));
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 3001, () => console.log("Server running"));
+  })
+  .catch((err) => console.error(err));
