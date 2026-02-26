d6fe4cf79bcac932ff37c8faea2576d0dbe82d5a
Grayson Gales <graysongales15@gmail.com>
2026-02-25 20:08:41 -0500

    Render deployment settings

--- server/index.js
+++ server/index.js
@@ -10,7 +10,20 @@ import courseRoutes from "./routes/courseRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_ORIGIN];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/v1/users", userRoutes);
